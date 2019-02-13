bdfa0708e915a8d744db2fd77bf492d33b21cf1b
oncebasun <basunjhm@sina.com>
2019-02-13 15:57:32 -0500

    update

--- code/default_config.py
+++ code/default_config.py
@@ -4,6 +4,8 @@ def get_default_config():
 
     # Data path
     args.datadir = '../data'
+    # Random seed
+    args.seed = None
     # Initial learning rate
     args.lr = 0.1
     # Store model of the best epoch
--- code/main.py
+++ code/main.py
@@ -6,9 +6,15 @@ import logging
 import datetime
 import argparse
 import importlib
+import torch
+import torchtext.data as data
 import default_config
 import configs.config as configuration
 import utils
+
+
+# TODO: design config setting system in a better way
+MODEL_CONFIGS = ['seed']
 
 
 def load_model_conf(model_dir, conf):
@@ -16,15 +22,23 @@ def load_model_conf(model_dir, conf):
         exec(f.read())
 
 
+def save_model_conf(model_dir, conf):
+    with open(os.path.join(model_dir, 'conf.txt'), 'w') as f:
+        for key in MODEL_CONFIGS:
+            if hasattr(conf, key):
+                f.write('args.%s = %s' %(key, getattr(conf, key)))
+
+
 if __name__ == '__main__':
     ###############################################
     #                 Preparation                 #
     ###############################################
-    # Configuration priority 1 > 2 > 3 > 4 > 5:
+    # Configuration priority 0 > 1 > 2 > 3 > 4 > 5:
+    # 0. (only for model configs) loaded model config
     # 1. command line options
     # 2. default command line options
     # 3. command line config file 
-    # 4. default config file 
+    # 4. main config file 
     # 5. defult 
     args = default_config.get_default_config()
     configuration.update_config(args)
@@ -35,8 +49,12 @@ if __name__ == '__main__':
     parser.add_argument('--test', action='store_true', default=False, help='train | test')
     parser.add_argument('--load', type=str, default=None, help='dir of model to load [default: None]')
     parser.add_argument('--debug', action='store_true', default=False, help='show DEBUG outputs')
+    # device
+    parser.add_argument('--device', type=str, default='cpu', help='device to use for iterate data. cpu | cudaX (e.g. cuda0) [default: cpu]')
     # data
     parser.add_argument('--datadir', '-d', type=str, help='path to the data directory [default: "%s"]' % args.datadir)
+    # model
+    parser.add_argument('--seed', type=int, help='manual seed [default: random seed or from config file]')
     # training
     parser.add_argument('-lr', type=float, metavar='FLOAT', help='initial learning rate [default: %f]' % args.lr)
     parser.add_argument('-best', action='store_true', help='store model of the best epoch [default: %s]' %str(args.best))
@@ -61,10 +79,6 @@ if __name__ == '__main__':
     # Run mode
     if not args.test:
         logger.info('Running in TRAINING mode.')
-        if args.best:
-            logger.info('Train in BEST mode')
-        else:
-            logger.info('Train in LAST mode')
         if hasattr(args, 'load') and args.load is not None:
             logger.info('Loading model: %s' % args.load)
             load_model_conf(args.load, args)
@@ -78,4 +92,40 @@ if __name__ == '__main__':
         logger.error('Running in Test mode and --load is not set')
         quit()
 
-    
+    # Device
+    if args.device == 'cpu':
+        args.cuda = False
+        logger.info('Device: CPU')
+    elif args.device[:4] == 'cuda':
+        if torch.cuda.is_available():
+            args.cuda = True
+            gpuid = int(args.device[4:])
+            torch.cuda.set_device(gpuid)
+            logger.info('Device: CUDA #%d' % gpuid)
+        else:
+            args.cuda = False
+            logger.warning('CUDA is not available now. Automatically switched to using CPU.')
+    else:
+        logging.error('Invalid device: %s !' % args.device)
+        quit()
+
+    # Seeding
+    if args.seed is None:
+        args.seed = random.randint(1, 100000)
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if args.cuda:
+        torch.cuda.manual_seed_all(args.seed)
+
+    # Show configurations
+    logger.info('%Configuration%')
+    for key in args.__dict__:
+        logger.info('  %s: %s' %(key, str(args.__dict__[key])))
+
+    # Save model configs
+    logger.info('Saving configs ... ')
+    save_model_conf(args.modeldir, args)
+
+    # Load dataset
+    text_field = data.Field(lower=True)
+    label_field = data.Field(sequential=True)
